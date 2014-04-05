be1f9a94970f1383b10a8641c3021bddcd4014b0
tosca ragnini <tosca.ragnini@gmail.com>
2014-04-05 17:25:29 -0500

    changed routing to index

--- Source/RickMaxwell/RickMaxwell.Web/App_Start/RouteConfig.cs
+++ Source/RickMaxwell/RickMaxwell.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace RickMaxwell.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Rick Maxwell_sculptor",
+                "Rick Maxwell_sculptor",
+                new { controller = "Home", action = "Index" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
