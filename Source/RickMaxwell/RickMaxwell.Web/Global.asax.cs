8d217071eaea2c444c20229430fbfc86f4a47849
tosca ragnini <tosca.ragnini@gmail.com>
2014-11-23 14:40:36 -0600

    comment out bundle table
    //   BundleTable.Bundles.RegisterTemplateBundles();

--- Source/RickMaxwell/RickMaxwell.Web/Global.asax.cs
+++ Source/RickMaxwell/RickMaxwell.Web/Global.asax.cs
@@ -43,7 +43,7 @@ namespace Web
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
 
-            BundleTable.Bundles.RegisterTemplateBundles();
+         //   BundleTable.Bundles.RegisterTemplateBundles();
         }
     }
 }
